e7b6a2aeb2fb38ea9b2260885613ec9deabe69f7
Martí Fenosa <marti.fenosa@gmail.com>
2023-06-23 12:42:33 +0200

    Update audio3d data

--- addons/three/audio-3d/src/main.ts
+++ addons/three/audio-3d/src/main.ts
@@ -1,14 +1,16 @@
 import { AddonData } from "@ft-engine/core";
 import { Vector3 } from "three";
-
-// Veure que falta aqui
-export interface Audio3DData {
+export interface AudioData {
     uid: string,
+    pan: number,
+    volume: number
+}
+export interface Audio3DData extends AudioData {
     innerRadius: number,
     outerRadius: number,
     position: Vector3
 }
 
 export interface AudioAddonData extends AddonData {
-    instances: Record<string, Audio3DData>
+    instances: Record<string, AudioData | Audio3DData>
 }
